da9765b39474494f5009a86db1e36260fe381b70
Jay Joshi <jayjoshi82694@gmail.com>
2025-11-09 06:45:24 -0800

    Build and project setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -51,10 +52,11 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
       open: true,
     },
   });
+  
